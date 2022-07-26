fd95042c47299e4a33841281c3049f79de873e56
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2022-07-26 10:07:44 -0300

    Initial commit

---
+++ src/style/globalstyle.jsx
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+	box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  html, body {
+    margin: 0;
+    padding: 0;
+
+    width: 100vw;
+    height: 100vh;
+    min-height: 100%;
+    min-height: -webkit-fill-available;    
+    
+    color: #fff;
+    background-color: #111;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
